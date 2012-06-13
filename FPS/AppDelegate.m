f261d96a921380f8836ccc278587fc3883408fef
Jacob Schwartz <jacob@cx.com>
2012-06-12 23:35:21 -0700

    Initial Commit

---
+++ FPS/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  FPS
+//
+//  Created by Jacob Schwartz on 12-06-12.
+//  Copyright (c) 2012 Jacob Schwartz. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ FPS/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FPS
+//
+//  Created by Jacob Schwartz on 12-06-12.
+//  Copyright (c) 2012 Jacob Schwartz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
